a8cba8ef0e8c1cd11abb241f55eb5fd15fdc083c
Dima Shabalin <dima.shabalin@ncloudtech.com>
2015-07-25 12:53:36 +0300

    updated

--- get_google_translation.rb
+++ get_google_translation.rb
@@ -5,12 +5,13 @@ query = ARGV[0]
 params = URI.escape(query)
 uri = URI("https://translate.google.ru/translate_a/single?client=t&sl=en&tl=ru&hl=en&dt=bd&dt=t&q=#{params}")
 response = Net::HTTP.get(uri)
-response.gsub!(",,",",")
+con = true
+con = response.gsub!(",,",",") while con
 h = JSON[response]
 
 str = ""
 arr = []
-if h.size > 2 then 
+if h.size > 2 then
   h[1].each {|word| arr += word[1]}
   if arr.size < 10 then
     str = arr.join("\n")
